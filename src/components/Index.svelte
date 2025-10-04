1a861ddebca3dcbd78b606e457f690c8cf81bcea
Russell Samora <russellsamora@gmail.com>
2025-10-04 17:23:43 -0400

    update github pages

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -4,12 +4,23 @@
 	const data = getContext("data");
 </script>
 
+<header>
+	<img src="assets/logo.png" alt="graze barrington logo" />
+
+	<h1>graze barrington</h1>
+
+	<p>
+		trying the most popular item at every food establishment in great barrington
+	</p>
+</header>
+
 <section id="places">
 	{#each data.places as p}
 		{@const img = p.name.replace(/\W/g, "").toLowerCase()}
 		{@const src = `assets/${img}@2x.jpg`}
 		{@const price = format("$,.2f")(p.cost)}
-		<div class="item">
+		{@const hide = !!p.hide}
+		<div class="item" class:hide>
 			<h2>{p.name}</h2>
 			<img {src} alt={"Image of " + p.item} />
 			<p>
@@ -37,8 +48,13 @@
 		border-bottom: 1px dashed var(--color-border);
 	}
 
+	.hide {
+		display: none;
+	}
+
 	h2 {
 		line-height: 1.2;
+		text-transform: lowercase;
 	}
 
 	p {
@@ -47,5 +63,19 @@
 
 	img {
 		margin: 16px auto;
+	}
+
+	header {
+		max-width: var(--col-width);
+		margin: 0 auto;
+	}
+
+	header img {
+		max-width: 256px;
+	}
+
+	h1 {
+		margin: 0 auto;
+		text-align: center;
 	}
 </style>
