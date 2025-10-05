5b1eea6d97c73970ccbcfb3039ce646aa4eb98a0
Russell Samora <russellsamora@gmail.com>
2025-10-05 13:57:41 -0400

    update github pages

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -14,16 +14,13 @@
 	</p>
 	<details>
 		<summary>about</summary>
-		<p>
-			there approximately 75 places that qualify, and i must only order the most
-			popular item according to the staff.
-		</p>
-		<p>food establishments must...</p>
+		<p>to qualify, a place must:</p>
 		<ul>
 			<li>sell to the public</li>
 			<li>prepare food on site</li>
 			<li>offer a savory meal</li>
 		</ul>
+		<p>at the time of writing, this yields approximately 75 places in town.</p>
 	</details>
 </header>
 
