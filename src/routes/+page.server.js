b8aaa5a5a233d3b9a2d8d9b4271a993399be1cdc
Russell Samora <russellsamora@gmail.com>
2025-11-07 20:22:12 -0500

    update github pages

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { descending } from "d3";
 import raw from "$data/places.csv";
 export async function load() {
-	const places = raw.filter((d) => d.item);
+	const places = raw.filter((d) => d.item && d.date);
 	places.sort((a, b) => descending(a.date, b.date));
 	return { places };
 }
